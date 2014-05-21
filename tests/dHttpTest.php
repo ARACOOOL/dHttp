9bf3cd25de3015c7e2f704d4863aa0bb34561ae9
ARACOOOL <tkdforever@gmail.com>
2014-05-21 11:55:18 +0500

    - using composer

--- tests/dHttpTest.php
+++ tests/dHttpTest.php
@@ -3,7 +3,7 @@
  * @author: Askar
  * @date: 20.05.13
  */
-include_once(__DIR__ . '/vendor/autoload.php');
+include_once(__DIR__ . '/../vendor/autoload.php');
 
 class dHttpTest extends PHPUnit_Framework_TestCase
 {

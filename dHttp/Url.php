8ad9b1c890d2f9ed42c55f2bd6e918b1771998c5
Askar <ARACOOOL@users.noreply.github.com>
2015-01-25 12:17:36 +0500

    Update Url.php

--- dHttp/Url.php
+++ dHttp/Url.php
@@ -69,8 +69,8 @@ class Url
 	 */
 	public static function cidr_match($ip, $cidr)
 	{
-		$ranges = (array)$cidr;
-		foreach ($ranges as $range) {
+		$cidr = (array)$cidr;
+		foreach ($cidr as $range) {
 			list($subnet, $mask) = explode('/', $range);
 			if ((ip2long($ip) & ~((1 << (32 - $mask)) - 1)) == ip2long($subnet)) {
 				return true;
